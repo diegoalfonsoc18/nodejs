32830953307fb2ee32aa304bc4758227092dbd94
Diego Alfonso <dlalfonso57@gmail.com>
2024-01-12 22:18:42 -0500

    my first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// const express = require("express");
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import indexRouter from "./routes/index.js";
+
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(join(__dirname, "views"));
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(indexRouter);
+
+app.listen(3000);
+console.log("Server is listening on port", 3000);
